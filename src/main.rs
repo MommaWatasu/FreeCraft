61e60b420e854389e675bc81ac2cb1dcde66a917
MommaWatasu <ascendwatson@gmail.com>
2022-07-26 13:31:38 +0900

    [add function or file]add cursor
    show cursor cross

--- src/main.rs
+++ src/main.rs
@@ -55,7 +55,9 @@ struct Player;
 fn setup_player(
     mut commands: Commands,
     mut windows: ResMut<Windows>,
-    asset_server: Res<AssetServer>
+    asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     let window = windows.get_primary_mut().unwrap();
     window.set_cursor_visibility(false);
@@ -86,13 +88,27 @@ fn setup_player(
                 // when you want to see your self, change the coordinate of z
                 transform: Transform::from_xyz(0.0, 0.5, 0.0),
                 ..default()
+            }).with_children(|camera| {
+                let plane = meshes.add(Mesh::from(shape::Plane{ size: 0.05 }));
+                let texture_handle = asset_server.load("cursor.png");
+                let material_handle = materials.add(StandardMaterial {
+                    base_color_texture: Some(texture_handle.clone()),
+                    alpha_mode: AlphaMode::Blend,
+                    unlit: true,
+                    ..default()
+                });
+                camera.spawn_bundle(PbrBundle {
+                    mesh: plane.clone(),
+                    material: material_handle.clone(),
+                    transform: Transform{
+                        translation: Vec3::new(0.0, 0.0, -1.0),
+                        rotation: Quat::from_rotation_x(std::f32::consts::PI / 2.0),
+                        ..default()
+                    },
+                    ..default()
+                }).insert(GlobalTransform::default());
             });
         });
-    
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("cursor.png"),
-        ..default()
-    });
 }
 
 fn to_radians(x: f32) -> f32 { x * PI / 180.0 }
